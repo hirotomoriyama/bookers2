28fdb80025cef16d9209b4c064bfcb62598e0034
hirotomoriyama <hiroto.moriyama7204@gmail.com>
2021-02-17 12:53:16 +0000

    [Add]4-10:ユーザ詳細、プロフィール編集機能を追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '0832cc77450cd536026e26ae4d4ba420626fb75f761aefd46576b0a690273cfe3768f08689b1898bb749f5eca95f321cad71548c85e12afea5551c17fd0ec928'
