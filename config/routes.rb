80ea2daebba65043d6b5edff86a1d3717cc7c926
hirotomoriyama <hiroto.moriyama7204@gmail.com>
2021-02-18 07:01:29 +0000

    [4-10]:Aboutページを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'books#index'
+  get "/home/about" => "home#about"
   devise_for :users
-  resources :books, only: [:new, :create, :index, :show, :destroy]
-  resources :users, only: [:show, :edit, :update]
+  resources :books
+  resources :users, only: [:index, :show, :edit, :update]
 end
