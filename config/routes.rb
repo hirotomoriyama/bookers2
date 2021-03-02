8052c41bfc9c94b67c2670f433b39fa782bd5c26
hirotomoriyama <hiroto.moriyama7204@gmail.com>
2021-03-02 10:05:26 +0000

    [Add]コメント機能のルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root to: 'home#top'
   get "/home/about" => "home#about"
   devise_for :users
-  resources :books, except: [:new]
   resources :users, only: [:index, :show, :edit, :update]
+  resources :books, except: [:new] do
+    resources :book_comments, only: [:create, :destroy]
+  end
 end
